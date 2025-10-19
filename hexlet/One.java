7ae1df34b52aa7826112a50d84711673b85edad6
user <user@MacBook-Air-user.local>
2025-10-19 20:48:17 +0300

    firt java progs

---
+++ hexlet/One.java
@@ -0,0 +1,7 @@
+package java_course.hexlet;
+
+public class One {
+    public static void main(String[] args) {
+        Two.greeting();
+    }
+}
---
+++ hexlet/Two.java
@@ -0,0 +1,7 @@
+package java_course.hexlet;
+
+public class Two {
+    public static void greeting() {
+        System.out.println("Java for Brave");
+    }
+}
